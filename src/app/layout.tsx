89b51d2d4934a23559beedf907a560bd133423c9
Sajjad Rasool <Sajjadr742@gmail.com>
2026-03-13 18:02:20 +0500

    feat: Update Microsoft Clarity ID to vv4qvtmjlx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -174,9 +174,9 @@ export default function RootLayout({
             __html: `
               (function(c,l,a,r,i,t,y){
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "vv4jg8zj52");
+              })(window, document, "clarity", "script", "vv4qvtmjlx");
             `,
           }}
         />
